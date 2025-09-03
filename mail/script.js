22aa1b08d09de5de492a1682589f71f9916bb902
Nicodemo1996 <nicodemoprincipe2@gmail.com>
2025-09-04 00:06:27 +0200

    [ADD] E-MAIL

--- mail/script.js
+++ mail/script.js
@@ -4,5 +4,19 @@ controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull’esito del controllo, utilizzando un ciclo for.
  Non è consentito usare nessun metodo proprio degli array (come includes, per esempio).
 Si può fare? Certo che si basta ragionare un po’.
-Nota: 
-Non è necessario provvedere alla validazione delle emai /*
+Nota:
+Non è necessario provvedere alla validazione delle emai */
+
+
+// Lista delle e-mail //
+
+const emailList = ["ginluca.fuscaldo@example.com", "lindita.lama@example.com", "diego.muto@example.com", "stefano.lombardo@example.com", "alfredo.lettieri@example.com", "giuseppe.vizza@example.com", "melina.larocca@example.com", "raffaele.scarola@example.com", "elisabetta.tangari@example.com"]
+
+//E-MAIL UTENTE//
+const emailUtente = prompt("Inserisci il tuo indirizzo email:");
+
+//BOOLEANO (FALSO)//
+let inList = false;
+
+//CONTROLLO UTENTE IN LISTA///
+
